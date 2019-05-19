cc80065c5cc5cf36c00b9fbe12665a03f31e74c9
unknown <jonknowles88@gmail.com>
2019-05-19 15:50:40 -0500

    Added additional operations and inner functions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-const largeArray = [];
-for (let i = 0; i < 1000000; i++) {
-  largeArray[i] = Math.floor(Math.random() * 100);
+const toNumArray = (length) => {
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    arr[i] = Math.floor(Math.random() * 100);
+  }
+  return arr;
+};
+
+const largeArray = toNumArray(100000);
+
+const nestedArray = [];
+for (let i = 0; i < 100; i++) {
+  nestedArray[i] = toNumArray(200);
 }
 
 const plusFive = x => x + 5;
 const greaterThan50 = x => x > 50;
+const lessThan2 = x => x < 2;
 const toSum = (acc, curr) => acc + curr;
+const toFlattened = (acc, curr) => acc.concat(curr);
 const addIndex = (e, i) => e + i;
 
 const withProfile = (fn, desc) => (...args) => {
+  let result;
   const start = new Date();
-  const result = fn(...args);
+  for (let i = 0; i < 100; i++) {
+    result = fn(...args);
+  }
   const duration = new Date() - start;
   console.log(`${desc}: ${duration}`);
   return result;
@@ -48,6 +63,20 @@ const forLoopFilter = pred => arr => {
   return resultArr;
 };
 
+const whileLoopFilter = pred => arr => {
+  const resultArr = [];
+  const _len = arr.length;
+  let i = 0;
+  while(i < _len) {
+    const e = arr[i];
+    if (pred(e, i, arr)) {
+      resultArr.push(e);
+    }
+    i++;
+  }
+  return resultArr;
+};
+
 const forLoopReduce = (fn, seed) => arr => {
   const _len = arr.length;
   let acc = seed === undefined
@@ -59,11 +88,52 @@ const forLoopReduce = (fn, seed) => arr => {
   return acc;
 };
 
+const whileLoopReduce = (fn, seed) => arr => {
+  const _len = arr.length;
+  let acc = seed === undefined
+    ? arr[0]
+    : seed;
+
+  let i = 0;
+  while(i < _len) {
+    acc = fn(acc, arr[i], i, arr);
+    i++;
+  }
+  return acc;
+};
+
 const forLoopForEach = fn => arr => {
   const _len = arr.length;
   for (let i = 0; i < _len; i++) {
     fn(arr[i], i, arr);
   }
+};
+
+const whileLoopForEach = fn => arr => {
+  const _len = arr.length;
+  let i = 0;
+  while (i < _len) {
+    fn(arr[i], i, arr);
+    i++;
+  }
+};
+
+const forLoopEvery = pred => arr => {
+  const _len = arr.length;
+  for (let i = 0; i < _len; i++) {
+    if(!pred(arr[i], i, arr)) return false;
+  }
+  return true;
+};
+
+const whileLoopEvery = pred => arr => {
+  const _len = arr.length;
+  let i = 0;
+  while(i < _len) {
+    if(!pred(arr[i], i, arr)) return false;
+    i++;
+  }
+  return true;
 };
 
 const arrayMap = (fn) => (arr) => {
@@ -82,31 +152,45 @@ const arrayForEach = (fn) => (arr) => {
   return arr.forEach(fn);
 };
 
-const use = iteratorFn => innerFn => arr => {
-  return iteratorFn(innerFn)(arr);
+const arrayEvery = (fn) => (arr) => {
+  return arr.every(fn);
 };
 
-console.log('map');
-console.group();
+const use = iteratorFn => (...firstArgs) => (...secondArgs) => {
+  return iteratorFn(...firstArgs)(...secondArgs);
+};
+
+console.group('map');
 withProfile(use(whileLoopMap)(plusFive), 'whileLoopMap')(largeArray);
 withProfile(use(forLoopMap)(plusFive), 'forLoopMap')(largeArray);
 withProfile(use(arrayMap)(plusFive), 'arrayMap')(largeArray);
 console.groupEnd();
 
-console.log('filter');
-console.group();
+console.group('filter');
+withProfile(use(whileLoopFilter)(greaterThan50), 'whileLoopFilter')(largeArray);
 withProfile(use(forLoopFilter)(greaterThan50), 'forLoopFilter')(largeArray);
 withProfile(use(arrayFilter)(greaterThan50), 'arrayFilter')(largeArray);
 console.groupEnd();
 
-console.log('reduce');
-console.group();
+console.group('reduce');
+console.log('toSum');
+withProfile(use(whileLoopReduce)(toSum, 0), 'whileLoopReduce')(largeArray);
 withProfile(use(forLoopReduce)(toSum, 0), 'forLoopReduce')(largeArray);
 withProfile(use(arrayReduce)(toSum, 0), 'arrayReduce')(largeArray);
+console.log('toFlattened');
+withProfile(use(whileLoopReduce)(toFlattened, []), 'whileLoopReduce')(nestedArray);
+withProfile(use(forLoopReduce)(toFlattened, []), 'forLoopReduce')(nestedArray);
+withProfile(use(arrayReduce)(toFlattened, []), 'arrayReduce')(nestedArray);
 console.groupEnd();
 
-console.log('forEach');
-console.group();
+console.group('forEach');
+withProfile(use(whileLoopForEach)(addIndex), 'whileLoopForEach')(largeArray);
 withProfile(use(forLoopForEach)(addIndex), 'forLoopForEach')(largeArray);
 withProfile(use(arrayForEach)(addIndex), 'arrayForEach')(largeArray);
+console.groupEnd();
+
+console.group('every');
+withProfile(use(whileLoopEvery)(lessThan2), 'whileLoopEvery')(largeArray);
+withProfile(use(forLoopEvery)(lessThan2), 'forLoopEvery')(largeArray);
+withProfile(use(arrayEvery)(lessThan2), 'arrayEvery')(largeArray);
 console.groupEnd();
